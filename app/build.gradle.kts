dc7556f3c4d696c24ea1cbdc55489ce7d254a4a5
EnesErdoğan <menes@armut.com>
2024-04-01 08:31:28 +0300

    feat : update build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,28 +3,35 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("dagger.hilt.android.plugin")
 }
 
 
 
 android {
     namespace = "com.app.weatherapplication"
-    compileSdk = 33
-
-    defaultConfig {
-        applicationId = "com.app.weatherapplication"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    compileSdk = 34
 
     val localProperties = Properties().apply {
         load(project.rootProject.file("local.properties").inputStream())
     }
     val apiKey: String = localProperties.getProperty("API_KEY", "")
+
+    defaultConfig {
+        applicationId = "com.app.weatherapplication"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "API_KEY", "\"$apiKey\"")
+        buildConfigField("String", "BASE_URL", "\"http://api.worldweatheronline.com/\"")
+    }
+
+
 
     buildTypes {
         release {
@@ -33,15 +40,16 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            buildConfigField("String", "API_KEY", "\"$apiKey\"")
-            buildConfigField("String", "BASE_URL", "http://api.worldweatheronline.com/")
+
         }
 
-        debug {
-            buildConfigField("String", "API_KEY", "\"$apiKey\"")
-            buildConfigField("String", "BASE_URL", "http://api.worldweatheronline.com/")
-        }
     }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -53,7 +61,6 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -70,14 +77,16 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
 
     //Hilt
-    implementation("com.google.dagger:hilt-android:2.44")
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt ("com.google.dagger:hilt-android-compiler:2.48")
+    kapt ("androidx.hilt:hilt-compiler:1.2.0")
 
     //Room
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
 
     //Ktx
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
@@ -86,5 +95,11 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
 
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    //coil
+    implementation("io.coil-kt:coil:2.6.0")
 }
 
